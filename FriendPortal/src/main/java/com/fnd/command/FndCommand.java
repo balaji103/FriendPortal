75ddefc72d524a1d2980a11be6a614b8bda1200b
npawa <email>
2018-11-21 23:07:28 +0530

    creating command class

---
+++ FriendPortal/src/main/java/com/fnd/command/FndCommand.java
@@ -0,0 +1,13 @@
+package com.fnd.command;
+
+public class FndCommand {
+	private int id;
+	private String fName;
+	private String lName;
+	private String education;
+	private String emailId;
+	private long mobNumber;
+	private String city;
+	private String companyName;
+
+}
